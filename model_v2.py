df73755723fae1d77aa3b1bd62d62390f5dc2d19
R v L <Rvanloenhout@hotmail.nl>
2024-03-06 11:18:38 +0100

    Optimized with new model

---
+++ model_v2.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+class CardClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super(CardClassifier, self).__init__()
+        
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        
+        # Max pooling layers
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(256 * 28 * 28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        x = x.view(-1, 256 * 28 * 28)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
